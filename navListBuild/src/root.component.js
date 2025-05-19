561f405b0ee7b258e0154360eb507036489a120c
Quan Nguyen Tho <thoquan150903@gmail.com>
2025-05-19 10:47:52 +0700

    .

--- navListBuild/src/root.component.js
+++ navListBuild/src/root.component.js
@@ -1,10 +1,41 @@
 import "./index.css";
 import { useEffect, useState } from "react";
 import "regenerator-runtime/runtime";
+import { FaEye } from "react-icons/fa";
+import { FaChevronDown, FaChevronRight } from "react-icons/fa6";
+import {
+  TbLayoutSidebarLeftCollapse,
+  TbLayoutSidebarLeftExpand,
+} from "react-icons/tb";
+import logo from "./assets/logo-evoerp.png";
 
 export default function Root() {
   const [id, setId] = useState();
   const [dataBuild, setDataBuild] = useState();
+
+  const [open, setOpen] = useState(true);
+  const [subMenus, setSubMenus] = useState({
+    calendar: false,
+    support: false,
+    tables: false,
+    analytics: false,
+  });
+
+  const toggleSubMenu = (menu) => {
+    setSubMenus((prev) => ({
+      ...prev,
+      [menu]: !prev[menu],
+    }));
+  };
+
+  const Menus = [
+    {
+      title: "Chế độ",
+      icon: <FaEye />,
+      subMenu: ["Chỉnh sửa", "Người dùng"],
+      key: "regime",
+    },
+  ];
 
   const handleChangeValue = (value, key) => {
     setDataBuild((prevData) => ({ ...prevData, [key]: value }));
@@ -48,37 +79,104 @@ export default function Root() {
   }, [id]);
 
   return (
-    <div className="bg-white px-10 py-2 flex justify-between items-center shadow-md">
-      <h1 className="text-2xl font-bold uppercase">
-        {dataBuild?.name || "Tên trang"}
-      </h1>
-      <select
-        className="bg-white border border-black rounded-md px-2 py-1 text-sm focus:outline-none w-48"
-        value={dataBuild?.mode}
-        onChange={(e) => handleChangeValue(e.target.value, "mode")}
-      >
-        <option value="" disabled>
-          Chọn chế độ
-        </option>
-        <option
-          className="bg-white hover:bg-indigo-700 hover:text-white rounded-md"
-          value="edit"
+    <>
+      <div className="w-full flex">
+        <div
+          className={`${
+            open ? "w-72 p-5" : "w-20 p-4"
+          } bg-indigo-700 h-screen pt-8 relative duration-300 ease-in-out`}
         >
-          Chế độ chỉnh sửa
-        </option>
-        <option
-          className="bg-white hover:bg-indigo-700 hover:text-white rounded-md"
-          value="user"
-        >
-          Chế độ người dùng
-        </option>
-        <option
-          className="bg-white hover:bg-indigo-700 hover:text-white rounded-md"
-          value="inactive"
-        >
-          Dừng hoạt động
-        </option>
-      </select>
-    </div>
+          <div
+            className={`absolute cursor-pointer -right-4 top-9 w-8 h-8 p-0.5 bg-zinc-50 border-zinc-50 border-2 rounded-full text-xl flex items-center justify-center ${
+              !open && "rotate-180"
+            } transition-all ease-in-out duration-300`}
+            onClick={() => setOpen(!open)}
+          >
+            {open ? (
+              <TbLayoutSidebarLeftExpand />
+            ) : (
+              <TbLayoutSidebarLeftCollapse />
+            )}
+          </div>
+
+          <div className="flex gap-x-4 items-center">
+            <img
+              src={logo}
+              alt="logo"
+              className={`w-10 h-10 rounded-full object-cover object-center cursor-pointer ease-in-out duration-3 ${
+                open && "rotate-[360deg]"
+              }`}
+            />
+            <h1
+              className={`text-zinc-50 origin-left text-lg duration-200 ease-in-out font-bold uppercase ${
+                !open && "scale-0"
+              }`}
+            >
+              {dataBuild?.name || "Tên trang"}
+            </h1>
+          </div>
+
+          <ul className="pt-6 space-y-0.5">
+            {Menus.map((Menu, index) => (
+              <li
+                key={index}
+                className={`flex flex-col rounded-md py-3 px-4 cursor-pointer hover:text-white text-zinc-50 hover:bg-zinc-800/50 transition-all ease-in-out duration-300 ${
+                  Menu.gap ? "mt-9" : "mt-2"
+                }`}
+                onClick={() => setOpen(true)}
+              >
+                <div
+                  className="flex items-center justify-between gap-x-4"
+                  onClick={() => toggleSubMenu(Menu.key)}
+                >
+                  <div className="flex items-center gap-2">
+                    <span className="text-lg">{Menu.icon}</span>
+                    <span
+                      className={`${
+                        !open && "hidden"
+                      } origin-left ease-in-out duration-300 font-semibold`}
+                    >
+                      {Menu.title}
+                    </span>
+                  </div>
+
+                  {Menu.subMenu && (
+                    <span
+                      className={`ml-auto cursor-pointer text-sm ${
+                        subMenus[Menu.key] ? "rotate-360" : ""
+                      } transition-transform ease-in-out duration-300 ${
+                        !open ? "hidden" : ""
+                      }`}
+                    >
+                      {subMenus[Menu.key] ? (
+                        <FaChevronDown />
+                      ) : (
+                        <FaChevronRight />
+                      )}
+                    </span>
+                  )}
+                </div>
+
+                {Menu.subMenu && subMenus[Menu.key] && (
+                  <ul className="pl-3 pt-4 text-zinc-300">
+                    {Menu.subMenu.map((subMenu, subIndex) => (
+                      <li
+                        key={subIndex}
+                        className="text-sm flex items-center gap-x-2 py-3 px-2 hover:bg-zinc-800 rounded-lg"
+                      >
+                        <span className="text-zinc-4">
+                          <FaChevronRight className="text-xs" />
+                        </span>
+                        {subMenu}
+                      </li>
+                    ))}
+                  </ul>
+                )}
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
+    </>
   );
 }
